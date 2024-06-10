09efb093bf39dc549454818639188ee8ff519c23
ray <audi3m@naver.com>
2024-06-10 19:27:22 +0900

    Initial Commit

---
+++ WeekTrend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeekTrend
+//
+//  Created by J Oh on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
